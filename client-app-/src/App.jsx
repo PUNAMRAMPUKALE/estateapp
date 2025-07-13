0114a0014b36736a114edf873dc1fe082e4a6ce7
PUNAMRAMPUKALE <p_pukale@u.pacific.edu>
2025-07-12 23:01:02 -0700

    home-page-design

--- client-app-/src/App.jsx
+++ client-app-/src/App.jsx
@@ -1,9 +1,16 @@
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar';
 import './layout.scss'
+import HomePage from './routes/homePage/HomePage.jsx';
 function App() {
   return (
     <div className="layout">
+      <div className="navbar">
       <Navbar/>
+      </div>
+      <div className="content">
+        <HomePage/>
+      </div>
+
       </div>
   )
 }
